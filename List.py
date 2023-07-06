d81f6921015dbaee2a5b6dc61dbb1ae3a87672aa
Sandip Jadhav <jadhavsandip1960@gmail.com>
2023-07-06 06:55:22 +0530

    Third Commit

---
+++ List.py
@@ -0,0 +1,26 @@
+# Make a list
+
+friends = ['Aniket','Chetan','Jivan','Mitesh','Pawan']
+print(friends)
+
+first_item = friends[0]
+print(first_item)
+
+# Get the last item in a list
+
+last_item = friends[-1]
+print(last_item)
+
+# Looping through a list
+
+for frd_name in friends:
+    print(frd_name)
+
+# Adding items to a list
+
+friends.append('Gudo_van')
+print(friends)
+
+friends.append('Tejas')
+friends.append('Komal')
+print(friends)
